b90d03a4435643402c5a118628d71123b22cb00c
Olutade Ajiboye <mrbigheaddd@gmail.com>
2018-02-09 14:47:44 +0100

    feat: reorganised scripts for ease of use.
    The GraphQuery script was made to house the functions that'll be used to POST requests to the graphQL server.

---
+++ Assets/graphQl-client/Scripts/ExampleQuery.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using graphQLClient;
+
+public class ExampleQuery : MonoBehaviour
+{
+
+	[Tooltip("This is the query call that gives me Pikachu's details")]
+	[TextArea]
+	public string getPokemonDetails;
+
+
+	string pokemonName = "Pikachu";
+	//Input parameters for both arrays and single variables.
+
+
+	void Start()
+	{
+		GraphQuery.url = "https://graphql-pokemon.now.sh/";
+		GetPikachuDetails(pokemonName);
+	}
+
+
+	public void GetPikachuDetails(string username)
+	{
+		GraphQuery.onQueryComplete += DisplayResult;
+		GraphQuery.variable["name"] = pokemonName;
+		GraphQuery.POST(getPokemonDetails);
+	}
+
+	public void DisplayResult()
+	{
+		Debug.Log(GraphQuery.queryReturn);
+	}
+
+	void OnDisable()
+	{
+		GraphQuery.onQueryComplete -= DisplayResult;
+	}
+}
--- Assets/graphQl-client/Scripts/GraphQuery.cs
+++ Assets/graphQl-client/Scripts/GraphQuery.cs
@@ -1,69 +1,135 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using SimpleJSON;
-using UnityEngine.UI;
 using UnityEngine;
 
-public class GraphQuery : MonoBehaviour
+namespace graphQLClient
 {
-	[Tooltip("The url of the node endpoint of the graphQL server being queried")] 
-	public string url;
-
-	public delegate void QueryComplete();
-	public static event QueryComplete onQueryComplete;
-
-
-	public enum Status { Neutral, Loading, Complete, Error };
-
-	public static Status queryStatus;
-	public static string queryReturn;
-
-
-	public class Query
+	public class GraphQuery : MonoBehaviour
 	{
-		public string query;
-	}
-	// Use this for initialization
-	public void Start()
-	{
+		public static GraphQuery instance = null;
+		[Tooltip("The url of the node endpoint of the graphQL server being queried")]
+		public static string url;
+
+		public delegate void QueryComplete();
+		public static event QueryComplete onQueryComplete;
 
 
-	}
+		public enum Status { Neutral, Loading, Complete, Error };
 
-	public WWW POST(string details)
-	{
-		Query query = new Query();
-		string jsonData = "";
-		WWWForm form = new WWWForm();
-		query = new Query { query = details };
-		jsonData = JsonUtility.ToJson(query);
-		byte[] postData = Encoding.ASCII.GetBytes(jsonData);
-		Dictionary<string, string> postHeader = form.headers;
-		if (postHeader.ContainsKey("Content-Type"))
-			postHeader["Content-Type"] = "application/json";
-		else
-			postHeader.Add("Content-Type", "application/json");
+		public static Status queryStatus;
+		public static string queryReturn;
 
-		WWW www = new WWW(url, postData, postHeader);
-		StartCoroutine(WaitForRequest(www));
-		queryStatus = Status.Loading;
-		return www;
-	}
 
-	IEnumerator WaitForRequest(WWW data)
-	{
-		yield return data; // Wait until the download is done
-		if (data.error != null)
+		public class Query
 		{
-			Debug.Log("There was an error sending request: " + data.error);
-			queryStatus = Status.Error;
+			public string query;
 		}
-		else
+
+		public void Awake()
 		{
-			queryReturn = data.text;
-			queryStatus = Status.Complete;
+			if (instance == null)
+			{
+				instance = this;
+			}
+			else if (instance != this)
+			{
+				Destroy(gameObject);
+			}
+
+            DontDestroyOnLoad(gameObject);
 		}
-		onQueryComplete();
+
+		public static Dictionary<string, string> variable = new Dictionary<string, string>();
+		public static Dictionary<string, string[]> array = new Dictionary<string, string[]>();
+		// Use this for initialization
+
+		public static WWW POST(string details)
+		{
+			details = QuerySorter(details);
+			Query query = new Query();
+			string jsonData = "";
+			WWWForm form = new WWWForm();
+			query = new Query { query = details };
+			jsonData = JsonUtility.ToJson(query);
+			byte[] postData = Encoding.ASCII.GetBytes(jsonData);
+			Dictionary<string, string> postHeader = form.headers;
+			if (postHeader.ContainsKey("Content-Type"))
+				postHeader["Content-Type"] = "application/json";
+			else
+				postHeader.Add("Content-Type", "application/json");
+
+			WWW www = new WWW(url, postData, postHeader);
+			instance.StartCoroutine(WaitForRequest(www));
+			queryStatus = Status.Loading;
+			return www;
+		}
+
+		static IEnumerator WaitForRequest(WWW data)
+		{
+			yield return data; // Wait until the download is done
+			if (data.error != null)
+			{
+				Debug.Log("There was an error sending request: " + data.error);
+				queryStatus = Status.Error;
+			}
+			else
+			{
+				queryReturn = data.text;
+				queryStatus = Status.Complete;
+			}
+			onQueryComplete();
+		}
+
+		public static string QuerySorter(string query)
+		{
+			string finalString;
+			string[] splitString;
+			string[] separators = { "$", "^" };
+			splitString = query.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+			finalString = splitString[0];
+			for (int i = 1; i < splitString.Length; i++)
+			{
+				if (i % 2 == 0)
+				{
+					finalString += splitString[i];
+				}
+				else
+				{
+					if (!splitString[i].Contains("[]"))
+					{
+						finalString += variable[splitString[i]];
+					}
+					else
+					{
+						finalString += ArraySorter(splitString[i]);
+					}
+				}
+			}
+			return finalString;
+		}
+
+		public static string ArraySorter(string theArray)
+		{
+			string[] anArray;
+			string solution;
+			anArray = array[theArray];
+			solution = "[";
+			foreach (string a in anArray)
+			{
+
+			}
+			for (int i = 0; i < anArray.Length; i++)
+			{
+				solution += anArray[i].Trim(new Char[] { '"' });
+				if (i < anArray.Length - 1)
+					solution += ",";
+			}
+			solution += "]";
+			Debug.Log("This is solution " + solution);
+			return solution;
+		}
 	}
 }
