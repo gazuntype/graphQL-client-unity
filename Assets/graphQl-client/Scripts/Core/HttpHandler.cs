d32781d28c1888bbe3090bea89ceeca6ab6ed373
Tade Ajiboye <mrbigheaddd@gmail.com>
2020-03-03 11:12:37 +0100

    init: set up httphandler

---
+++ Assets/graphQl-client/Scripts/Core/HttpHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQlClient.EventCallbacks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace GraphQlClient.Core
+{
+	public class HttpHandler
+	{
+		public static async Task<UnityWebRequest> PostAsync(string url, string details, string authToken = null){
+            byte[] postData = Encoding.ASCII.GetBytes(details);
+            UnityWebRequest request = UnityWebRequest.Post(url, UnityWebRequest.kHttpVerbPOST);
+            request.uploadHandler = new UploadHandlerRaw(postData);
+            request.SetRequestHeader("Content-Type", "application/json");
+            if (!String.IsNullOrEmpty(authToken)) 
+                request.SetRequestHeader("Authorization", "Bearer " + authToken);
+            OnRequestBegin  requestBegin = new OnRequestBegin();
+            requestBegin.FireEvent();
+            try{
+                await request.SendWebRequest();
+            }
+            catch(Exception e){
+                Debug.Log("Testing exceptions");
+                OnRequestEnded requestFailed = new OnRequestEnded(e);
+                requestFailed.FireEvent();
+            }
+			Debug.Log(request.downloadHandler.text);
+            OnRequestEnded requestSucceeded = new OnRequestEnded(request.downloadHandler.text);
+            requestSucceeded.FireEvent();
+            return request;
+        }
+
+        public static async Task<UnityWebRequest> GetAsync(string url, string authToken){
+            UnityWebRequest request = UnityWebRequest.Get(url);
+            if (!String.IsNullOrEmpty(authToken)) 
+                request.SetRequestHeader("Authorization", "Bearer " + authToken);
+            OnRequestBegin  requestBegin = new OnRequestBegin();
+            requestBegin.FireEvent();
+            try{
+                await request.SendWebRequest();
+            }
+            catch(Exception e){
+                Debug.Log("Testing exceptions");
+                OnRequestEnded requestEnded = new OnRequestEnded(e);
+                requestEnded.FireEvent();
+            }
+            Debug.Log(request.downloadHandler.text);
+            OnRequestEnded requestSucceeded = new OnRequestEnded(request.downloadHandler.text);
+            requestSucceeded.FireEvent();
+            return request;
+        }
+	}
+}
---
+++ Assets/graphQl-client/Scripts/EventCallbacks/AllEventInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GraphQlClient.EventCallbacks
+{
+    #region Network
+
+    public class OnRequestBegin : Event<OnRequestBegin>
+    {
+        public OnRequestBegin(){
+            
+        }
+    }
+
+    public class OnRequestEnded : Event<OnRequestEnded>
+    {
+        public string data;
+        public bool success;
+        public Exception exception;
+        public OnRequestEnded(string data){
+            this.data = data;
+            success = true;
+        }
+
+        public OnRequestEnded(Exception exception){
+            this.exception = exception;
+            success = false;
+        }
+    }
+
+    #endregion
+}
