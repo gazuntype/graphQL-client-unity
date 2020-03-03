bf4aff19f83997b4cff7971e0cd6e556cd8d736e
Tade Ajiboye <mrbigheaddd@gmail.com>
2020-03-03 21:18:30 +0100

    feat: can chose fields from return type

--- Assets/graphQl-client/Scripts/Core/GraphApi.cs
+++ Assets/graphQl-client/Scripts/Core/GraphApi.cs
@@ -1,5 +1,9 @@
-﻿using Boo.Lang;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using GraphQlClient.Core;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -10,37 +14,123 @@ namespace GraphQlClient.Core
     {
         public string url;
 
-        public List<Query> queries = new List<Query>();
-        public List<Mutation> mutations = new List<Mutation>();
+        public List<Query> queries;
 
-        private string introspection =
-            "{\n  __schema {\n    queryType {\n      name\n      fields{\n        name\n      }\n    }\n    mutationType{\n      name\n      fields{\n        name\n      }\n    }\n  }\n}";
+        public List<Mutation> mutations;
+        
+        private string introspection;
 
+        private string introspectionQuery;
+        
         public async void Introspect(){
-            UnityWebRequest request = await HttpHandler.PostAsync(url, introspection);
+            introspectionQuery =
+                "{\n  __schema {\n      queryType {\n        fields{\n          name\n          type{\n            name\n            kind\n            ofType {\n              kind\n              name\n            }\n          }\n        }\n      }\n  }\n}";
+            UnityWebRequest request = await HttpHandler.PostAsync(url, introspectionQuery);
+            introspection = request.downloadHandler.text;
         }
 
         public void CreateNewQuery(){
-            queries.Add(new Query());
+            if (queries == null)
+                queries = new List<Query>();
+            Query query = new Query();
+            JObject jObject = JObject.Parse(introspection);
+            JArray jArray = (JArray) jObject["data"]["__schema"]["queryType"]["fields"];
+            query.fields = new List<Field>();
+            query.queryOptions = new List<string>();
+            for (int i = 1; i < jArray.Count; i++){
+                query.queryOptions.Add(jArray[i]["name"].ToString());
+            }
+
+            queries.Add(query);
+        }
+        
+        public void GetQueryReturnType(Query query, int index){
+            JObject jObject = JObject.Parse(introspection);
+            JArray jArray = (JArray) jObject["data"]["__schema"]["queryType"]["fields"];
+            string fieldName;
+            switch (jArray[index+1]["type"]["kind"].ToString()){
+                case "OBJECT":
+                    fieldName = jArray[index+1]["type"]["name"].ToString();
+                    break;
+                case "LIST":
+                    fieldName = jArray[index+1]["type"]["ofType"]["name"].ToString();
+                    break;
+                default:
+                    fieldName = jArray[index+1]["type"]["name"].ToString();
+                    break;
+            }
+
+            query.returnType = fieldName;
+        }
+
+        public Field CreateSubFields(Query query, string type){
+            Field field = query.fields.Find((aField => aField.name == type));
+            IntrospectType(query, type, field);
+            return field;
+        }
+
+        private async void IntrospectType(Query query, string type, Field parent = null){
+            string queryText =
+                $"{{\n__type(name: \"{type}\"){{\n    name\n    fields{{\n      name\n      type{{\n        name\n            kind\n        ofType{{\n          name\n        }}\n      }}\n    }}\n  }}\n}}";
+            UnityWebRequest request = await HttpHandler.PostAsync(url, queryText);
+            string result = request.downloadHandler.text;
+            JObject jObject = JObject.Parse(result);
+            JArray jArray = (JArray) jObject["data"]["__type"]["fields"];
+            for (int i = 0; i < jArray.Count; i++){
+                string fieldType;
+                switch (jArray[i]["type"]["kind"].ToString()){
+                    case "OBJECT":
+                        fieldType = jArray[i]["type"]["name"].ToString();
+                        break;
+                    case "LIST":
+                        fieldType = jArray[i]["type"]["ofType"]["name"].ToString();
+                        break;
+                    case "NON_NULL":
+                        fieldType = jArray[i]["type"]["ofType"]["name"].ToString();
+                        break;
+                    default:
+                        fieldType = jArray[i]["type"]["name"].ToString();
+                        break;
+                }
+                Field field = new Field{name = jArray[i]["name"].ToString(),type = fieldType, parent = parent, index = query.fields.Count};
+                query.fields.Add(field);
+            }
         }
 
         public void DeleteQuery(int index){
             queries.RemoveAt(index);
         }
 
+        public void DeleteAllQueries(){
+            queries = new List<Query>();
+        }
+
         #region Classes
 
+        [Serializable]
         public class Query
         {
-            public string queryName;
+            public string name;
+            public string queryString;
+            public string returnType;
+            public List<string> queryOptions;
+            public List<Field> fields;
         }
 
         public class Mutation
         {
-            public string mutationName;
+            public string name;
+        }
+
+        public class Field
+        {
+            public int index;
+            public string name;
+            public string type;
+            public Field parent;
+            public List<Field> children;
         }
 
         #endregion
     }
 }
-
--- Assets/graphQl-client/Scripts/Editor/GraphApiEditor.cs
+++ Assets/graphQl-client/Scripts/Editor/GraphApiEditor.cs
@@ -1,4 +1,7 @@
-﻿using GraphQlClient.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraphQlClient.Core;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,9 +10,15 @@ namespace GraphQlClient.Editor
     [CustomEditor(typeof(GraphApi))]
     public class GraphApiEditor : UnityEditor.Editor
     {
+        private int index;
         public override void OnInspectorGUI()
         {
             GraphApi graph = (GraphApi)target;
+            if (GUILayout.Button("Reset")){
+                graph.DeleteAllQueries();
+            }
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
             EditorGUILayout.LabelField(graph.name);
             graph.url = EditorGUILayout.TextField("Url", graph.url);
             if(GUILayout.Button("Introspect")){
@@ -31,16 +40,42 @@ namespace GraphQlClient.Editor
             EditorGUILayout.Space();
             EditorGUILayout.Space();
 
-            if (graph.queries.Count > 0)
-                EditorGUILayout.LabelField("Queries");
-            for (int i = 0; i < graph.queries.Count; i++){
-                graph.queries[i].queryName = EditorGUILayout.TextField("Query Name", graph.queries[i].queryName);
-                if (GUILayout.Button("Delete")){
-                    graph.DeleteQuery(i);
+            if (graph.queries != null){
+                if (graph.queries.Count > 0)
+                    EditorGUILayout.LabelField("Queries");
+                for (int i = 0; i < graph.queries.Count; i++){
+                    GraphApi.Query query = graph.queries[i];
+                    query.name = EditorGUILayout.TextField("Query Name", query.name);
+                    string[] options = query.queryOptions.ToArray();
+                    if (String.IsNullOrEmpty(query.returnType)){
+                        index = EditorGUILayout.Popup("Query", index, options);
+                        query.queryString = options[index];
+                        EditorGUILayout.LabelField(options[index]);
+                        if (GUILayout.Button("Create Field")){
+                            graph.GetQueryReturnType(query, index);
+                            graph.CreateSubFields(query, query.returnType);
+                        }
+                    }
+                    else{
+                        EditorGUILayout.LabelField(query.queryString,
+                            $"Return Type: {query.returnType}");
+                    }
+
+                    List<GraphApi.Field> fields = query.fields.FindAll((aField => aField.parent == null));
+                    if (fields.Count > 0){
+                        string[] returnTypeFields = fields.Select(o => o.name).ToArray();
+                        EditorGUILayout.Popup("Query", 0, returnTypeFields);
+                    }
+                    
+                        
+                    if (GUILayout.Button("Delete")){
+                        graph.DeleteQuery(i);
+                    }
                 }
             }
-            
-            
+
+
+
             EditorUtility.SetDirty(graph);
         }
     }
